896b791c9c90dcada0628152f45eb79724f334c8
JivkoKelchev <jivko.r.kelchev@gmail.com>
2019-06-01 21:07:26 +0300

    remove fontawesome and change current agro year bug fixed

--- src/RozzBundle/Services/LandsService.php
+++ src/RozzBundle/Services/LandsService.php
@@ -48,33 +48,12 @@ class LandsService
 
     public function setUsedAreaForActiveContracts( ObjectManager $em ,\DateTime $startOfAgroYear){
         $allUsedArea = $em->getRepository(UsedArea::class)->findAll();
-        $contractStatusArray = $this->container->getParameter('contracts_status_array');
         foreach ($allUsedArea as $usedArea){
             $contract=$usedArea->getContract();
 
             if ($this->contractService->isContractActive($em, $contract->getId())){
 
                 $usedArea->setActive(1);
-
-//            /**
-//             * @var Contracts $contract
-//             */
-//            //договора е изтекъл
-//            if ($contract->getExpire() < $startOfAgroYear and
-//                ($contract->getStatus() != $contractStatusArray['has_annex'] ))
-//            {
-//                $usedArea->setActive(0);//неактивна площ
-//                $contract->setStatus($contractStatusArray['expired']);//изтекъл срок
-//            }
-//            //има анекс
-//            elseif( $contract->getStatus() ==  $contractStatusArray['has_annex'])
-//            {
-//                $usedArea->setActive(0);//не активна площ
-//            }
-//            else{
-//                $usedArea->setActive(1);//активна площ
-//                $contract->setStatus($contractStatusArray['active']);
-//            }
 
             }else{
                 $usedArea->setActive(0);
